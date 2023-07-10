91987bef7f8fea49690131c2050f056ae6fdc432
Coyotefrank <franklopez35789@gmail.com>
2023-07-09 22:12:54 -0700

    made alot of proggress

--- Develop/script.js
+++ Develop/script.js
@@ -3,8 +3,77 @@
 // in the html.
 //THIS FUNCTION WILL LOAD AS SOON AS THE PAGE LOADS
 $(function () {
+  const now = dayjs().hour()
+  const today = dayjs().format()
+  $("#currentDay").text(today)
+  console.log(now) 
   // TODO: Add a listener for click events on the save button.
   //CREAT AN EVENT LISTNER THAT WILL SAVE THE TEXT TO LOCAL STORAGE
+$(".saveBtn").on("click", function(event){
+  event.preventDefault() 
+let storageItem = localStorage.getItem("schedual") ? JSON.parse(localStorage.getItem("schedual")) : []
+console.log($(this))
+const row =$(this).parent().attr("id")
+const task = $(this).siblings("textarea").val()
+storageItem.push({row,task})
+localStorage.setItem("schedual", JSON.stringify(storageItem))
+})
+function createRows() { 
+  let storageItem = JSON.parse(localStorage.getItem("schedual")) 
+
+  for (let i = 9; i < 19; i++) {
+    let rowBlock = $("<div>")
+    rowBlock.addClass("row time-block").attr("id","hour-"+ i)
+    let rowBLocktextArea = $("<textarea>")
+    rowBLocktextArea.addClass("col-8 col-md-10 description")
+    let hourBlock = $("<div>")
+    hourBlock.addClass("col-2 col-md-1 hour text-center py-3").text(dayjs().hour(i).format("h A"))
+    let btn = $("<button>")
+    btn.addClass("btn saveBtn col-2 col-md-1").attr("aria-label","save").html('<i class="fas fa-save" aria-hidden="true"></i>')
+    rowBlock.append(hourBlock).append(rowBLocktextArea).append(btn)
+    $(".container-lg").append(rowBlock)
+    if (storageItem && storageItem.length) {
+      for (const iterator of storageItem) {
+        console.log(iterator)
+        if (iterator.row===rowBlock.attr("id")) {
+          rowBLocktextArea.val(iterator.task)
+        }
+      }
+
+      //rowBLocktextArea.val()
+    }
+  }
+  
+}
+// $("body .time-block").each(function(){
+//   const rowColors =$(this).attr("id").split("-")[1]
+//   console.log (rowColors)
+//   if (parseInt(rowColors)==now) {
+//     $(this).addClass("future")
+
+//   } else if (parseInt(rowColors)<now) {
+//     $(this).addClass("future")
+//   } else {
+//     $(this).addClass("past")
+//   }
+// })
+createRows()
+function updateHours() {
+  console.log("updated");
+  $(".time-block").each(function () {
+   let blockHour = parseInt($(this).attr("id")); //get rows ids as block hours
+
+   //adding classes based on time of the day
+   if (now > blockHour) {
+    $(this).addClass("past");
+   } else if (now === blockHour) {
+    $(this).addClass("present");
+   } else {
+    $(this).addClass("future");
+   }
+  });
+ }
+ updateHours();
 
   //TARGET THE VALUE BY ID 
   //when the program starts we want to add a css class based on what time it is right now
@@ -21,6 +90,7 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+    //add
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
